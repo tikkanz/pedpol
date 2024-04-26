ef04bce00aef4420ef60978a000277ed67143e8f
Ric Sherlock <ric.sherlock@lic.co.nz>
2024-04-26 16:26:39 +1200

    Add get_ancestors and get_descendents

--- src/pedigree/generations.py
+++ src/pedigree/generations.py
@@ -1,7 +1,73 @@
+from typing import Collection
+
 import polars as pl
 
 from pedigree.core import PedigreeLabels, parents
 
+
+def get_progeny_of(
+    df: pl.DataFrame | pl.LazyFrame,
+    ids: pl.Expr | Collection[any] | pl.Series,
+    pedigree_labels: tuple[str, str, str] = PedigreeLabels,
+) -> pl.DataFrame | pl.LazyFrame:
+    """Return the progeny of the animals specified"""
+    return df.filter(pl.any_horizontal(pl.col(pedigree_labels[1:]).is_in(ids)))
+
+
+def get_parents_of(
+    df: pl.DataFrame | pl.LazyFrame,
+    ids: pl.Expr | Collection[any] | pl.Series,
+    pedigree_labels: tuple[str, str, str] = PedigreeLabels,
+) -> pl.DataFrame | pl.LazyFrame:
+    """Return the parents of the animals specified"""
+    animal, sire, dam = pedigree_labels
+    anims = df.filter(pl.col(animal).is_in(ids))
+    return df.filter(pl.col(animal).is_in(anims.select(parents((sire, dam)))))
+
+def get_descendents_of(
+    df: pl.DataFrame | pl.LazyFrame,
+    ids: pl.Expr | Collection[any] | pl.Series,
+    generations: int | None = None,
+    include_ids: bool = True,
+    pedigree_labels: tuple[str, str, str] = PedigreeLabels,
+) -> pl.DataFrame | pl.LazyFrame:
+    """Return the descendents of the animals specified"""
+    animal = pedigree_labels[0]
+    ids = pl.DataFrame({animal: ids}, schema=df.select(animal).schema)
+    ids_descendants = pl.DataFrame(schema=ids.schema)
+    g = 0
+    ids_g = ids
+    while generations > g and ids_g.height != 0:
+        ids_g = get_progeny_of(df, ids_g, pedigree_labels).select(animal)
+        ids_descendants = pl.concat([ids_descendants, ids_g])
+        g += 1
+
+    if include_ids:
+        ids_descendants = pl.concat([ids_descendants, ids])
+    return df.filter(pl.col(animal).is_in(ids_descendants))
+
+
+def get_ancestors_of(
+    df: pl.DataFrame | pl.LazyFrame,
+    ids: pl.Expr | Collection[any] | pl.Series,
+    generations: int | None = None,
+    include_ids: bool = True,
+    pedigree_labels: tuple[str, str, str] = PedigreeLabels,
+) -> pl.DataFrame | pl.LazyFrame:
+    """Return the ancestors of the animals specified"""
+    animal = pedigree_labels[0]
+    ids = pl.DataFrame({animal: ids}, schema=df.select(animal).schema)
+    ids_ancestors = pl.DataFrame(schema=ids.schema)
+    g = 0
+    ids_g = ids
+    while generations > g and ids_g.height != 0:
+        ids_g = get_parents_of(df, ids_g, pedigree_labels).select(animal)
+        ids_ancestors = pl.concat([ids_ancestors, ids_g])
+        g += 1
+
+    if include_ids:
+        ids_ancestors = pl.concat([ids_ancestors, ids])
+    return df.filter(pl.col(animal).is_in(ids_ancestors))
 
 def classify_generations(
     df: pl.DataFrame | pl.LazyFrame,
--- tests/pedigree/test_generation.py
+++ tests/pedigree/test_generation.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 import polars as pl
 from pedigree.core import null_unknown_parents
-from pedigree.generations import classify_generations
+from pedigree.generations import classify_generations, get_descendents_of
 
 data_dir = Path("/home/rishe0/dev/pedigree/tests/resources")
 
-ped = pl.read_csv(
+pedjv = pl.read_csv(
     data_dir / "ped_jv.csv", dtypes=3 * [pl.Int32], comment_prefix="#"
 ).pipe(
     null_unknown_parents,
@@ -16,10 +16,19 @@ pedlit = pl.read_csv(
     data_dir / "ped_literal.csv", dtypes=3 * [pl.Utf8], comment_prefix="#"
 ).pipe(null_unknown_parents, parent_labels=("Father", "Mother"))
 
-ped = ped.pipe(classify_generations, pedigree_labels=("progeny", "sire", "dam"))
+pedjv = pedjv.pipe(classify_generations, pedigree_labels=("progeny", "sire", "dam"))
 pedlit = pedlit.pipe(
     classify_generations, pedigree_labels=("Child", "Father", "Mother")
 )
 
-print(ped)
+ids = [3, 4]
+get_descendents_of(
+    pedjv,
+    ids,
+    generations=6,
+    include_ids=True,
+    pedigree_labels=("progeny", "sire", "dam"),
+)
+
+print(pedjv)
 print(pedlit)
