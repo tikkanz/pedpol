6858b6dabaf91877dd7cf571bd95ef4ade94d2b3
Ric Sherlock <ric.sherlock@lic.co.nz>
2024-04-26 12:23:25 +1200

    Assume that the pedigree represents unknown parents as `null`
    The `null_unknown_parents` function can be used to preprocess the pedigree appropriately.

--- src/pedigree/generations.py
+++ src/pedigree/generations.py
@@ -2,11 +2,14 @@ import polars as pl
 
 
 def is_integer(df, column):
+    """Determines if the specified column in the DataFrame has an integer type"""
     return isinstance(df.schema[column], (pl.Int64, pl.Int32, pl.Int16))
 
 
 def get_unknown_parent_value(df, parent_column):
-    """Determines if"""
+    """Determines the value used to represent unknown sires/dams
+
+    Assumes `0` if parent Ids are integers, or `'.'` if the parent Ids are literals."""
     map_null = {True: 0, False: "."}
     return map_null[is_integer(df, parent_column)]
 
@@ -19,7 +22,7 @@ def null_unknown_parents(
     parent_labels: tuple[str, str] = ("sire", "dam"),
     unknown_parent_value=None,
 ) -> pl.LazyFrame | pl.DataFrame:
-    """Replaces the parent ID used to represent 'unknown' with null"""
+    """Replaces the parent Id used to represent 'unknown' with null"""
     if unknown_parent_value is None:
         unknown_parent_value = get_unknown_parent_value(df, parent_labels[0])
     return df.with_columns(
@@ -33,26 +36,16 @@ def null_unknown_parents(
     )
 
 
-def parents(parent_labels: tuple[str, str] = ("sire", "dam"), null_value=0) -> pl.Expr:
+def parents(parent_labels: tuple[str, str] = ("sire", "dam")) -> pl.Expr:
     """Returns an expression describing the parents in a pedigree"""
     sire, dam = parent_labels[0], parent_labels[-1]
-    return (
-        pl.col(sire)
-        .filter(pl.col(sire) != null_value)
-        .append(pl.col(dam).filter(pl.col(dam) != null_value))
-        .unique()
-        .alias("parents")
-    )
+    return pl.col(sire).append(pl.col(dam)).drop_nulls().unique().alias("parents")
 
 
 def get_parents(
-    df: pl.LazyFrame | pl.DataFrame,
-    parent_labels: tuple[str, str] = ("sire", "dam"),
-    null_value=None,
+    df: pl.LazyFrame | pl.DataFrame, parent_labels: tuple[str, str] = ("sire", "dam")
 ) -> pl.LazyFrame | pl.DataFrame:
-    if null_value is None:
-        null_value = get_unknown_parent_value(df, parent_labels[0])
-    return df.select(parents(parent_labels=parent_labels, null_value=null_value))
+    return df.select(parents(parent_labels=parent_labels))
 
 
 def classify_generations(
--- tests/pedigree/test_generation.py
+++ tests/pedigree/test_generation.py
@@ -1,12 +1,24 @@
 from pathlib import Path
 
 import polars as pl
-from pedigree.generations import classify_generations
+from pedigree.generations import classify_generations, null_unknown_parents
 
 data_dir = Path("/home/rishe0/dev/pedigree/tests/resources")
 
-ped = pl.read_csv(data_dir / "ped_jv.csv", comment_prefix="#")
+ped = pl.read_csv(
+    data_dir / "ped_jv.csv", dtypes=3 * [pl.Int32], comment_prefix="#"
+).pipe(
+    null_unknown_parents,
+)
+
+pedlit = pl.read_csv(
+    data_dir / "ped_literal.csv", dtypes=3 * [pl.Utf8], comment_prefix="#"
+).pipe(null_unknown_parents, parent_labels=("Father", "Mother"))
 
 ped = ped.pipe(classify_generations, pedigree_labels=("progeny", "sire", "dam"))
+pedlit = pedlit.pipe(
+    classify_generations, pedigree_labels=("Child", "Father", "Mother")
+)
 
 print(ped)
+print(pedlit)
